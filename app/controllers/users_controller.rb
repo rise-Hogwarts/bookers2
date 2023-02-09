17cf3c1727e608cd5b70c7cad7adeea1ee78fd88
EC2 Default User <ec2-user@ip-172-31-2-119.ap-northeast-1.compute.internal>
2023-02-09 16:17:36 +0000

    [save]bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,10 @@ class UsersController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
+  post 'users/:id/edit' => 'users#edit', as: 'edit'
   get 'home/about' => 'homes#about', as: 'about'
   resources :books, only: [:index, :show, :edit, :create, :update, :destroy]
   resources :users, only: [:index, :show, :edit, :update]
